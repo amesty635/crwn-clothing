1c9b0cae72ca72da4da623d72da38a12e6e0c4fd
amesty635 <gcie.ea@gmail.com>
2023-03-28 15:09:15 +0300

    feat: firebase connection, user sign-in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Directory from "./components/directory/directory.component";
+import {Routes, Route} from "react-router-dom";
+import Home from "./components/routes/home/home.component";
+import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
 
+
+const Shop = () => {
+    return (
+        <div>I am shop page</div>
+    )
+}
 
 const App = () => {
-return (
-    <div>
-        <Directory/>
-    </div>
-)
-
+    return (
+        <Routes>
+            <Route path='/' element={<Navigation/>}>
+                <Route index element={<Home/>}/>
+                <Route path='shop' element={<Shop/>}/>
+                <Route path='sign-in' element={<SignIn/>}/>
+            </Route>
+        </Routes>
+    )
 }
 
 export default App;
